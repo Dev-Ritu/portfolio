b8241ee76a0dc6c3851744bc9a121826390616db
Ritu <ritikasinha9958@gmail.com>
2024-06-17 21:39:37 +0530

    My first Commit

---
+++ src/Details.js
@@ -0,0 +1,138 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/projects/RITU_IMG.png";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+import nodejs from "./assets/techstack/nodejs.png";
+import restApi from "./assets/techstack/resAPi.png";
+import agGrid from "./assets/techstack/aggrid.png";
+import sourceTree from "./assets/techstack/sourcetree.png";
+import fork from "./assets/techstack/fork.png";
+
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+import tavaTrip from "./assets/projects/taav-trip.png";
+import tavaCloud from "./assets/projects/tava_cloud.png";
+import understory from "./assets/projects/understory.png";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Ritu",
+  tagline: "I build things for web",
+  img: profile,
+  about: `I'm an innovative and results-driven Software Developer specializing in front-end development, user interface design, and project management. Proficient in React.js, Tailwind, CSS, JavaScript, HTML, Bootstrap, Redux, Material UI, and Git. Skilled in UX/UI principles, Figma design, and optimizing code for enhanced software performance. I excel in troubleshooting, cross-functional collaboration, and efficient task management, ensuring timely delivery of top-notch software solutions.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/ritu-kumari-39208120b",
+  github: "https://github.com/Dev-Ritu",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Software Developer",
+    Company: `Innostax Software Lab Pvt Lmt`,
+    Location: "Gurugram",
+    Type: "Full Time",
+    Duration: "Aug 2022 - Present",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Computer Science and Engineering",
+    Company: "YMCA University, Faridabad",
+    Location: "Haryana",
+    Type: "Regular",
+    Duration: "Aug 2019 - May 2022",
+  },
+  {
+    Position: "Computer Engineering",
+    Company: `Govt. Polytechnic For Women, Faridabd`,
+    Location: "Haryana",
+    Type: "Regular",
+    Duration: "Aug 2016 - May 2019",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  github: github,
+  figma: figma,
+  nodejs: nodejs,
+  fork: fork,
+  sourceTree,
+  restApi,
+  agGrid,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Tava Cloud",
+    image: tavaCloud,
+    description: `Developed a no-code platform with intuitive drag-and-drop nodes for endpoint design, and created
+frontend app templates for seamless user development`,
+    techstack: "HTML/CSS, React, Redux, tailwind.css",
+    previewLink: "https://tava.cloud/",
+  },
+  {
+    title: "Tava Trip",
+    image: tavaTrip,
+    description: `Built a comprehensive flight and hotel booking application with user-friendly interfaces and
+robust search functionalities, optimizing performance for a seamless booking experience`,
+    techstack: "React, Redux, tailwind.css",
+    previewLink: "https://www.tavatrip.com/",
+  },
+  {
+    title: "Under-Story",
+    image: understory,
+    description: `Contributed to the development of insurance solutions tailored for the climate era`,
+    techstack: "CSS, react-native for web, context API",
+    previewLink: "https://understoryweather.com/",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "ritikasinha9958@gmail.com",
+  phone: "+91 7011315746",
+};
---
+++ src/Pages/Technologies.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { techStackDetails } from "../Details";
+
+function Technologies() {
+  const {
+    html,
+    css,
+    js,
+    react,
+    redux,
+    tailwind,
+    bootstrap,
+    vscode,
+    github,
+    npm,
+    postman,
+    figma,
+    restApi,
+    fork,
+    nodejs,
+    sourceTree,
+    agGrid
+  } = techStackDetails;
+  return (
+    <main className="container mx-auto max-width pt-10 pb-20 ">
+      <section>
+        <h1 className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
+          Tech Stack
+        </h1>
+        <p className="text-content py-2 lg:max-w-3xl">
+          Technologies I've been working with recently
+        </p>
+      </section>
+      <section className="grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 items-center gap-10 pt-6">
+        <img src={html} title="html" alt="" />
+        <img src={css} title="CSS" alt="" />
+        <img src={js} title="JavaScript" alt="" />
+        <img src={react} title="React" alt="" />
+        <img src={redux} title="Redux" alt="" />
+        <img src={tailwind} title="Tailwind CSS" alt="" />
+        <img src={bootstrap} title="Bootstrap" alt="" />
+        <img src={nodejs} title="Node.js" alt="node.js" />
+        <img src={restApi} title="Rest APIs" alt="rest-api" />
+        <img src={agGrid} title="Ag-Grid" alt="ag-grid" />
+      </section>
+      <section>
+        <h1 className="text-2xl pt-10 text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
+          Tools
+        </h1>
+      </section>
+      <section className="grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 items-center gap-10 pt-6">
+        <img src={vscode} title="Visual Studio Code" alt="" />
+        <img src={github} title="Github" alt="Github" />
+        <img src={figma} title="Figma" alt="Figma" />
+        <img src={sourceTree} title="Source-tree" alt="source-tree" height={100} width={100} />
+        <img src={fork} title="Fork" alt="fork" />
+        <img src={npm} title="NPM" alt="NPM" />
+        <img src={postman} title="Postman" alt="Postman" />
+      </section>
+    </main>
+  );
+}
+
+export default Technologies;
